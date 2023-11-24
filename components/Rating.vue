6d20aabc0f6ace9ade7671a18314124d4d2175b1
Satrio Bagus <satrio.baguswbw@gmail.com>
2023-11-25 02:34:59 +0700

    add star to Icon comp & add Rating comp

---
+++ components/Rating.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+const props = defineProps ({
+  rating: Number,
+})
+</script>
+<template>
+  <div class="flex">
+    <Icon v-for="_ in props.rating" name="star" :size="20" class="text-yellow-400" />
+    <Icon v-for="_ in 5-props.rating" name="star" :size="20" class="text-neutral-500" />
+  </div>
+</template>
