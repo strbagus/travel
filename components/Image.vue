aa65800a725fe59e47f516e3b4512fd31b1f44c2
Satrio Bagus <satrio.baguswbw@gmail.com>
2023-11-26 04:19:36 +0700

    Edit component Icon & Image

--- components/Image.vue
+++ components/Image.vue
@@ -4,11 +4,12 @@ const props = defineProps({
   class: String,
   src: String,
   alt: String,
+  loading: String,
 })
 </script>
 <template>
   <div class="relative w-full" :class="props.class">
-    <img :src="props.src" :alt="props.alt" class="absolute object-cover h-full w-full left-0 right-0">
+    <img :src="props.src" :loading="props.loading" :alt="props.alt" class="absolute object-cover h-full w-full left-0 right-0">
     <div class="h-0 opacity-0 group-hover:h-full group-hover:opacity-10 bg-black"></div>
   </div>
 </template>
